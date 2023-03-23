4f40a10b8d2a86bc128475030894928621d5f926
Mina Nur Cebi <mina.cebi@bites.com.tr>
2023-03-23 18:34:27 +0300

    calisma9 eklendi.

---
+++ bolum4/calisma9/calisma9/calisma9.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    cout << a << "\n"; //standart output monitör
+
+    cin >> a; //klavyeden okunan değeri a'ya atar. //standart input klavye
+    cout << "klavyeden " << a << " degerini girdiniz."<< "\n";
+    cout << "klavyeden girilen degerin 10 fazlasi " << a+10 << "\n";
+
+    return 0;
+}

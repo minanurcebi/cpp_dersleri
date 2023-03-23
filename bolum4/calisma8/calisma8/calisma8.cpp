605d662da4a3bf9d2a9c57f52167efa834654654
Mina Nur Cebi <mina.cebi@bites.com.tr>
2023-03-23 18:25:12 +0300

    calisma8 eklendi.

---
+++ bolum4/calisma8/calisma8/calisma8.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    cout << a << "\n";
+
+    int b = a << 2; //left shift -- a'nın 2 sola kaydırılmış halini b'ye atıyoruz.(sağdan 2 sıfır ekle)
+    cout << b << "\n";
+
+    int c = b >> 3; //right shift -- b'nin 3 sola kaydırılmış halini c'ye ata.(sağdan 3 sıfır sil)
+    cout << c << "\n";
+
+    int x = 22 & 13;
+    cout << x << "\n";
+    
+    x = 22 ^ 13;
+    cout << x << "\n";
+
+    x = 22 | 13; 
+    cout << x << "\n";
+
+    return 0;
+}

8dee82e3fe52581f1ae2358464afde8224b30266
Mina Nur Cebi <mina.cebi@bites.com.tr>
2023-03-23 16:07:54 +0300

    calisma4 eklendi.

---
+++ bolum4/calisma4/calisma4/calisma4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;  //tip, isim
+    a = 10; //deger
+    cout << a << endl;
+    a = 20; // "=" :atama(assignment)
+    cout << "Hello World!\n";
+    int b = 30; //Her bir cümle (statement)
+    int c = 40; int d = 50; //iki satırı tek satırda yazdık.
+    int x = 60, y = 70, z{};
+
+    cout << b << "\n";  //endl yerine "\n" yazmak daha performanslı
+    cout << c << "\n";
+    cout << d << "\n";
+    cout << x << "\n";
+    cout << y << "\n";
+    cout << z << "\n";
+    return 0;
+}

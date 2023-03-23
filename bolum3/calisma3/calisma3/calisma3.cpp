15fe4fe0512c571c9b1f00d8c047bbdc3e37f2c7
Mina Nur Cebi <mina.cebi@bites.com.tr>
2023-03-23 15:00:00 +0300

    calisma3 eklendi.

---
+++ bolum3/calisma3/calisma3/calisma3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+//comment (yorum satiri)
+//tek satirli (single line)
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+/*cok
+satirli
+yorumlar
+*/

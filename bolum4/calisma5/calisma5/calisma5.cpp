aa16668f45bcd4e72d89491e0c9af97b2325660d
Mina Nur Cebi <mina.cebi@bites.com.tr>
2023-03-23 16:53:04 +0300

    calisma5 eklendi.

---
+++ bolum4/calisma5/calisma5/calisma5.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    cout << a << "\n";
+    float pi = 3.14;
+    cout << pi << "\n";
+    long tl = 23534265232252;
+    cout << tl << "\n";
+    char c = 'x';
+    cout << c << "\n";
+    return 0;
+}
